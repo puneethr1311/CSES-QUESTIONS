19806c1ea27ecb525d98a8df31b92bcde7d3eecf
PUNEET <136790768+puneethr1311@users.noreply.github.com>
2025-10-13 19:00:55 +0530

    Add files via upload

---
+++ Introductory_Problems/gray_code.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+                    
+using namespace std ; 
+void fn(vector<string>&ans,int m ){
+    for(int i = m-1;i>=0;i--){
+        ans.push_back(ans[i]);
+    }
+}
+                 
+int main(){ 
+    int n ;
+    cin>>n;
+    string str = "";
+    vector<string>ans ;
+    ans.push_back("0");
+    ans.push_back("1");
+
+    int m = 2;
+    for(int i =1;i<n;i++){
+        fn(ans,m);
+        m = m*2;
+        int j = 0;
+        while(j<m/2){
+            ans[j] = '0' + ans[j];
+            j++;
+        }
+        while(j<m){
+            ans[j] = '1'+ans[j];
+            j++;
+        }
+    }
+    for(int i = 0;i<m;i++)cout<<ans[i]<<endl;
+
+    return 0 ;
+}
